1369c542521fbbdc704fbd71034c3bdae7f1341d
Genny <145250839+gennyofficial@users.noreply.github.com>
2024-11-03 07:49:07 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Password-Impossible/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Password-/" : "/",
   chainWebpack: config => {
     config.resolve.set('fallback', {
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
